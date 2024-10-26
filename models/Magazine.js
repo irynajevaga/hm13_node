30e438f3020daa7357cb3f9f7416ad796e018357
irynajevaga <163996627+irynajevaga@users.noreply.github.com>
2024-10-26 23:28:31 +0200

    Initial commit

---
+++ models/Magazine.js
@@ -0,0 +1,10 @@
+import { Schema, model, Types } from 'mongoose'
+
+const magazineSchema = new Schema({
+  title: { type: String, required: true },
+  issueNumber: { type: Number, required: true },
+  publisher: { type: Schema.Types.ObjectId, ref: "Publisher" },
+});
+
+
+export const Magazine = model("Magazine", magazineSchema);
